0344c476d9960548235dc3458ebbee779be0b786
Adeon18 <ostaptrush18@gmail.com>
2021-10-25 17:04:33 +0300

    Changed another warning.

--- src/main/java/store/SepalLengthFilterSpecification.java
+++ src/main/java/store/SepalLengthFilterSpecification.java
@@ -3,7 +3,6 @@ package store;
 import flowerstore.Flower;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class SepalLengthFilterSpecification extends FlowerFilterSpecification{
     private double sepalLength;
@@ -14,7 +13,7 @@ public class SepalLengthFilterSpecification extends FlowerFilterSpecification{
 
     @Override
     protected ArrayList<Flower> ApplyFilter(ArrayList<Flower> flowers) {
-        ArrayList<Flower> filtered_flowers = new ArrayList<Flower>();
+        ArrayList<Flower> filtered_flowers = new ArrayList<>();
 
         for (Flower flower : flowers) {
             if (flower.getSepalLength() == sepalLength) {
